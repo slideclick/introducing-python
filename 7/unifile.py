67886e51c6e7766012b27f02a7901b96bbd834e7
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2016-02-23 11:11:41 +0800

    一个文件，它存的是字节。Rb模式读入内存，它是bytes
    如果是r模式，那么它读入内存时，需要先解码：
    f= open('Capture.PNG','r', encoding='ascii',errors='replace')
    到了内存里面，它是str
    
    这个str你repr它，会看到最外层的引号，表示它是str字面量
    
    然后这个str还可以eval成python的obj

--- 7/unifile.py
+++ 7/unifile.py
@@ -1,8 +1,18 @@
-# abcdefgh穿山甲到
+# abcdefgh穿山甲到?
 s=b'\xe7\xa9\xbf\xe5\xb1\xb1\xe7\x94\xb2\xe5\x88\xb0\xe7\xa9\xbf\xe5\xb1\xb1\xe7\x94\xb2\xe5\x88\xb0\xe5\xba\x95\xe8\xaf\xb4\xe4\xba\x86\xe4\xbb\x80\xe4\xb9\x88\xef\xbc\x9f'
 sd = s.decode('utf-8') 
 print(sd)
 
+with open('chinese.txt','wb') as fb:
+    fb.write(s)#我用二进制方式写进文件，那么以UFT8打开就可以正确显示
+
+with open('chinese.txt','r') as fb:
+    st= fb.read()#read会失败，以OS默认的gbk去解码会失败
+
+with open('chinese.txt','r',encoding='utf-8') as fb:
+    st= fb.read()#read会成功，因为我指定了解码格式是utf-8   
+    
+    
 import re
 rs = re.search(r'(a)\w+\1',"cabad") 
 if rs:
