caa8e98616c6bae18810548e4197c10521fad5a9
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2016-02-23 11:45:31 +0800

    latin-1 is a direct mapping of bytes 0-255 to Unicode characters U+0000 to U+00FF. latin-1 encoding is notable in that it will never produce a decoding error when reading text of a possibly unknown encoding. Reading a file as latin-1 might not produce a completely correct text decoding, but it still might be enough to extract useful data out of it. Also, if you later write the data back out, the original input data will be preserved.

--- 7/unifile.py
+++ 7/unifile.py
@@ -15,7 +15,9 @@ with open('chinese.txt','r',encoding='utf-8') as fb:
     st= fb.read()#read会成功，因为我指定了解码格式是utf-8   
     #如果你在控制台print st那个东东，那个unicode的st又会被编码为gbk去给微软console
     
-    
+s='\xe7\xa9\xbf\xe5\xb1\xb1\xe7\x94\xb2\xe5\x88\xb0\xe5\xba\x95\xe8\xaf\xb4\xe4\xba\x86\xe4\xbb\x80\xe4\xb9\x88\xef\xbc\x9f' 
+s.encode('latin-1').decode('utf-8') 
+ 
 import re
 rs = re.search(r'(a)\w+\1',"cabad") 
 if rs:
