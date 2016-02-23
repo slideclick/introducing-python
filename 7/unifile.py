bc159a727b4c2e759c384a0ac6fee20077382d6c
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2016-02-23 11:37:13 +0800

    s=b'\xe7\xa9\xbf\xe5\xb1\xb1\xe7\x94\xb2\xe5\x88\xb0\xe7\xa9\xbf\xe5\xb1\xb1\xe7\x94\xb2\xe5\x88\xb0\xe5\xba\x95\xe8\xaf\xb4\xe4\xba\x86\xe4\xbb\x80\xe4\xb9\x88\xef\xbc\x9f' #当你上行那么写时，'\xe7'是四个ascii字符，写入py源码文件中。而本文第一行的四个中文是二进制 'e7'写入py文件 #当python解释器读到s=b''这个写法时，它解释器负责把\xe7这四个字符，理解为二进制的e7

--- 7/unifile.py
+++ 7/unifile.py
@@ -1,5 +1,7 @@
 # abcdefgh穿山甲到?
 s=b'\xe7\xa9\xbf\xe5\xb1\xb1\xe7\x94\xb2\xe5\x88\xb0\xe7\xa9\xbf\xe5\xb1\xb1\xe7\x94\xb2\xe5\x88\xb0\xe5\xba\x95\xe8\xaf\xb4\xe4\xba\x86\xe4\xbb\x80\xe4\xb9\x88\xef\xbc\x9f'
+#当你上行那么写时，'\xe7'是四个ascii字符，写入py源码文件中。而本文第一行的四个中文是二进制 'e7'写入py文件
+#当python解释器读到s=b''这个写法时，它解释器负责把\xe7这四个字符，理解为二进制的e7
 sd = s.decode('utf-8') 
 print(sd)
 print()
@@ -11,6 +13,7 @@ with open('chinese.txt','r') as fb:
 
 with open('chinese.txt','r',encoding='utf-8') as fb:
     st= fb.read()#read会成功，因为我指定了解码格式是utf-8   
+    #如果你在控制台print st那个东东，那个unicode的st又会被编码为gbk去给微软console
     
     
 import re
@@ -34,7 +37,7 @@ if rs:
     print(rs.group(0))
     print(rs.group(1))
     print(rs.group(2))
-    print(rs.group(3))
+
     print(rs.groups())
     print(rs.groupdict())    
     
